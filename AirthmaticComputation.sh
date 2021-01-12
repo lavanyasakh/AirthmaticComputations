666bd5bf2c8d5f0591b80b89b8adb13f1e74400a
lavanya <lavanyace929@gmail.com>
2021-01-12 20:26:02 +0530

    Added:Store the results in Dictionary

--- AirthmaticComputation.sh
+++ AirthmaticComputation.sh
@@ -5,7 +5,7 @@ read -p "Enter value of b: " b1
 read -p "Enter value of c: " c1
 
 
-result=$(( $a1 + $b1 * $c1 ))
+result1=$(( $a1 + $b1 * $c1 ))
 echo $result1
 
 read -p "Enter value of a: " a2
@@ -13,7 +13,7 @@ read -p "Enter value of b: " b2
 read -p "Enter value of c: " c2
 
 
-result=$(( $a2 * $b2 + $c2 ))
+result2=$(( $a2 * $b2 + $c2 ))
 echo $result2
 
 read -p "Enter value of a: " a3
@@ -21,7 +21,7 @@ read -p "Enter value of b: " b3
 read -p "Enter value of c: " c3
 
 
-result=$(( $c3 + $a3/$b3 ))
+result3=$(( $c3 + $a3/$b3 ))
 echo $result3
 
 read -p "Enter value of a: " a4
@@ -29,6 +29,15 @@ read -p "Enter value of b: " b4
 read -p "Enter value of c: " c4
 
 
-result=$(( $a4%$b4 + $c4 ))
+result4=$(( $a4%$b4 + $c4 ))
 echo $result4
+
+declare -A TotalList
+
+TotalList[compu1]=$result1
+TotalList[compu2]=$result2
+TotalList[compu3]=$result3
+TotalList[compu4]=$result4
+
+echo ${TotalList[@]}
 
