e482f1a18aa7a184124ebee6c57e837b2d91a447
lavanya <lavanyace929@gmail.com>
2021-01-12 19:49:13 +0530

    Added:Computation2

--- AirthmaticComputation.sh
+++ AirthmaticComputation.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
 
-read -p "Enter value of a: " a
-read -p "Enter value of b: " b
-read -p "Enter value of c: " c
+read -p "Enter value of a: " a1
+read -p "Enter value of b: " b1
+read -p "Enter value of c: " c1
 
 
-result=$(( $a + $b * $c ))
-echo $result
+result=$(( $a1 + $b1 * $c1 ))
+echo $result1
+
+read -p "Enter value of a: " a2
+read -p "Enter value of b: " b2
+read -p "Enter value of c: " c2
+
+
+result=$(( $a2 * $b2 + $c2 ))
+echo $result2
+
